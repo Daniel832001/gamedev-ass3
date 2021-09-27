619844c5c9f49cf12fded5ffa315ccd43ed5574d
Dan <daniel.heffernan8301@gmail.com>
2021-09-27 20:20:44 +1000

    added code to auto level generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,6 +6,7 @@ public class LevelGenerator : MonoBehaviour
 {
     private GameObject[] mapLayouts;
     private int[,] levelMap;
+    private Transform[] mapPieces;
     public Sprite emptySpace;
     public Sprite outsideCorner;
     public Sprite outsideWall;
@@ -40,8 +41,84 @@ public class LevelGenerator : MonoBehaviour
             { 2,2,2,2,2,1,5,3,3,0,4,0,0,0},      
             { 0,0,0,0,0,0,5,0,0,0,4,0,0,0},    
         };
-    }
+        //to the right 1.276
+        //down 3.72
+        mapPieces = GetComponentsInChildren<Transform>();
+        for (int i = 0; i< levelMap.GetLength(0); i++)
+        {
 
+            for (int j = 0; j < levelMap.GetLength(1); j++)
+            {
+                int mapPiece = levelMap[i,j];
+                Transform currentPiece = Instantiate(mapPieces[mapPiece+1], new Vector3(j*1.276f, i*-1.276f, 0f), Quaternion.identity);
+                switch (mapPiece+1)
+                {
+                    case 2:
+                        Corner(North(i,j),East(i,j),South(i,j),West(i,j),true);
+                    case 3:
+                        Wall(North(i, j), East(i, j), South(i, j), West(i, j),true);
+                    case 4:
+                        Corner(North(i, j), East(i, j), South(i, j), West(i, j),false);
+                    case 5:
+                        Wall(North(i, j), East(i, j), South(i, j), West(i, j),false);
+                }
+            }
+        }
+        //Instantiate(mapPieces[1], new Vector3(0f, 0f, 0f), Quaternion.identity);
+        //Instantiate(mapPieces[2], new Vector3(1.276f, 0f, 0f), Quaternion.identity);
+    }
+    public int North(int i, int j)
+    {
+        if (i == 0)
+        {
+            return 10;
+        }
+        else
+        {
+            return levelMap[i - 1, j];
+        }
+    }
+    public int East(int i, int j)
+    {
+        if (j == 13)
+        {
+            return 10;
+        }
+        else
+        {
+            return levelMap[i, j+1];
+        }
+    }
+    public int South(int i, int j)
+    {
+        if (i == 14)
+        {
+            return 10;
+        }
+        else
+        {
+            return levelMap[i + 1, j];
+        }
+    }
+    public int West(int i, int j)
+    {
+        if (j == 0)
+        {
+            return 10;
+        }
+        else
+        {
+            return levelMap[i, j-1];
+        }
+    }
+    public void Corner(int north, int east, int south, int west, bool outside)
+    {
+
+    }
+    public void Wall(int north, int east, int south, int west, bool outside)
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
