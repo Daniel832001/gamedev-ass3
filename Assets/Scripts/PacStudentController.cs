27f7e4c9d1f8147f327cb98a52e60bab9b17a647
Dan <daniel.heffernan8301@gmail.com>
2021-11-04 17:12:12 +1100

    created a way to identify specific tiles within the map, and began implimenting it into the pacstudent movement script

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -9,11 +9,23 @@ public class PacStudentController : MonoBehaviour
     private bool lerping;
     private string currentInput;
     private Tweener tweener;
+    private GameObject pacStudent;
+    private int currentRow = 1;
+    private int currentCol = 1;
+    private List<Row> rows = new List<Row>();
 
     // Start is called before the first frame update
     void Start()
     {
         tweener = GetComponent<Tweener>();
+        pacStudent = GameObject.FindWithTag("PacStudent");
+
+        for (int i = 0; i<29; i++)
+        {
+            GameObject currentRow = GameObject.FindWithTag("Row " + i.ToString());
+            Row row = new Row(currentRow.transform.GetComponentsInChildren<Transform>());
+            rows.Add(row);
+        }
     }
 
     // Update is called once per frame
@@ -36,29 +48,23 @@ public class PacStudentController : MonoBehaviour
             lastInput = "D";
         }
 
-        lerping = Lerping();
-
-        if (!lerping)
+        //if not lerping start new lerp
+        if (!tweener.TweenExists(pacStudent.transform))
         {
             StartCoroutine(Move());
         }
-        Debug.Log(lastInput);
-    }
-
-    bool Lerping()
-    {
-
-        return false;
     }
 
     private IEnumerator Move()
     {
         string attemptedDirection = Direction();
+        //if its possible to move in current direction or last input create new lerp
         if (!attemptedDirection.Equals(""))
         {
             currentInput = attemptedDirection;
+            UpdateLocation();
 
-            Vector3 targetPosittion = GetNewPosition();
+            Vector3 targetPosittion = rows[currentRow].cols[currentCol].position;
             AddItem(targetPosittion, GetTime(targetPosittion));
         }
         yield return null;
@@ -66,33 +72,100 @@ public class PacStudentController : MonoBehaviour
 
     string Direction()
     {
-        //if (lastInput)
-        //{
-        //    return lastInput;
-        //}else if (currentInput)
-        //{
-        //    return currentInput;
-        //}
+        if (CanMove(lastInput))
+        {
+            return lastInput;
+        }else if (CanMove(currentInput))
+        {
+            return currentInput;
+        }
 
         return "";
     }
 
-    Vector3 GetNewPosition()
+    bool CanMove(string direction)
     {
-        //currentInput;
-        return new Vector3(0, 0, 0);
+        switch (direction)
+        {
+            case "W":
+                if (currentRow < 28)
+                {
+                    if (rows[currentRow + 1].cols[currentCol].name.Contains("layout_5")
+                    || rows[currentRow + 1].cols[currentCol].name.Contains("layout_0")
+                    || rows[currentRow + 1].cols[currentCol].name.Contains("power pellet"))
+                    {
+                        return true;
+                    }
+                }                
+                break;
+            case "S":
+                if (currentRow > 0)
+                {
+                    if (rows[currentRow - 1].cols[currentCol].name.Contains("layout_5")
+                    || rows[currentRow - 1].cols[currentCol].name.Contains("layout_0")
+                    || rows[currentRow - 1].cols[currentCol].name.Contains("power pellet"))
+                    {
+                        return true;
+                    }
+                }
+                break;
+            case "D":
+                if (currentCol < 28)
+                {
+                    if (rows[currentRow].cols[currentCol + 1].name.Contains("layout_5")
+                    || rows[currentRow].cols[currentCol + 1].name.Contains("layout_0")
+                    || rows[currentRow].cols[currentCol + 1].name.Contains("power pellet"))
+                    {
+                        return true;
+                    }
+                }
+                break;
+            case "A":
+                if (currentCol > 0)
+                {
+                    if (rows[currentRow].cols[currentCol - 1].name.Contains("layout_5")
+                    || rows[currentRow].cols[currentCol - 1].name.Contains("layout_0")
+                    || rows[currentRow].cols[currentCol - 1].name.Contains("power pellet"))
+                    {
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
     }
-    float GetTime(Vector3 distance)
+    void UpdateLocation()
     {
+        switch (currentInput)
+        {
+            case "W":
+                currentRow += 1;
+                break;
+            case "S":
+                currentRow -= 1;
+                break;
+            case "D":
+                currentCol += 1;
+                break;
+            case "A":
+                currentCol -= 1;
+                break;
+        }
+    }
+
+
+    float GetTime(Vector3 position)
+    {
+
         return 0f;
     }
 
     private bool AddItem(Vector3 position, float time)
     {
-        //if (tweener.AddTween(item.transform, item.transform.position, position, time))
-        //{
-        //    return true;
-        //}
+        if (tweener.AddTween(pacStudent.transform, pacStudent.transform.position, position, time))
+        {
+            return true;
+        }
         return false;
     }
 }
